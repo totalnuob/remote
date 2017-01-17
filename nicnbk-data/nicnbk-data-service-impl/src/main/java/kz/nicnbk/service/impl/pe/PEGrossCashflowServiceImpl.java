1d434241630f3d4fdd11173f3148c30cbda74df2
magzumov <magzumov@gmail.com>
2017-01-17 19:17:29 +0600

    PE rename

--- nicnbk-data/nicnbk-data-service-impl/src/main/java/kz/nicnbk/service/impl/pe/PEGrossCashflowServiceImpl.java
+++ nicnbk-data/nicnbk-data-service-impl/src/main/java/kz/nicnbk/service/impl/pe/PEGrossCashflowServiceImpl.java
@@ -1,6 +1,5 @@
 package kz.nicnbk.service.impl.pe;
 
-import kz.nicnbk.repo.api.pe.PEGrossCashflowRepository;
 import kz.nicnbk.repo.model.pe.PEGrossCashflow;
 import kz.nicnbk.service.api.pe.PEGrossCashflowService;
 import kz.nicnbk.service.converter.pe.PEGrossCashflowEntityConverter;
