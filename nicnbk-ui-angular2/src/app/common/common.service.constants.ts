550bd5ced0b91236d7cf42403cd29149155ccb95
magzumov <magzumov@gmail.com>
2017-01-18 09:34:40 +0600

    Merge branch 'master' into production
    # Conflicts:
    #	nicnbk-common/nicnbk-common-service-api/src/main/java/kz/nicnbk/common/service/model/PageableResult.java
    #	nicnbk-data/nicnbk-data-service-api/src/main/java/kz/nicnbk/service/dto/tripMemo/TripMemoPagedSearchResult.java
    #	nicnbk-data/nicnbk-data-service-api/src/main/java/kz/nicnbk/service/dto/tripmemo/TripMemoPagedSearchResult.java

--- nicnbk-ui-angular2/src/app/common/common.service.constants.ts
+++ nicnbk-ui-angular2/src/app/common/common.service.constants.ts
@@ -1,4 +1,4 @@
-//export const DATA_APP_URL = "http://app.nicnbk.kz:8089/";
-export const DATA_APP_URL = "http://localhost:8089/";
+export const DATA_APP_URL = "http://app.nicnbk.kz:8089/";
+//export const DATA_APP_URL = "http://localhost:8089/";
 //export const DATA_APP_URL = "http://10.10.163.45:8089/";
 
