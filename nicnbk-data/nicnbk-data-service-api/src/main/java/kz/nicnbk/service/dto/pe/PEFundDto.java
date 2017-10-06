0ffdc1ebca73edc5c0d354c40eaaebc6d1d818f9
Pak <alexey.v.pak@gmail.com>
2017-10-06 11:25:46 +0600

    My new message 3

--- nicnbk-data/nicnbk-data-service-api/src/main/java/kz/nicnbk/service/dto/pe/PEFundDto.java
+++ nicnbk-data/nicnbk-data-service-api/src/main/java/kz/nicnbk/service/dto/pe/PEFundDto.java
@@ -40,15 +40,15 @@ public class PEFundDto extends HistoryBaseEntityDto<PEFund> {
     private String nonsuitableReason;
 
     //KEY FUND STATISTICS
-    private int numberOfInvestments;
-    private double investedAmount;
-    private double realized;
-    private double unrealized;
-    private double dpi;
-    private double netIrr;
-    private double netTvpi; // MOIC
-    private double grossIrr;
-    private double grossTvpi;
+    private Integer numberOfInvestments;
+    private Double investedAmount;
+    private Double realized;
+    private Double unrealized;
+    private Double dpi;
+    private Double netIrr;
+    private Double netTvpi; // MOIC
+    private Double grossIrr;
+    private Double grossTvpi;
     private Date asOfDate;
     private Double benchmarkNetIrr;
     private Double benchmarkNetTvpi;
@@ -229,75 +229,75 @@ public class PEFundDto extends HistoryBaseEntityDto<PEFund> {
         this.geography = geography;
     }
 
-    public int getNumberOfInvestments() {
+    public Integer getNumberOfInvestments() {
         return numberOfInvestments;
     }
 
-    public void setNumberOfInvestments(int numberOfInvestments) {
+    public void setNumberOfInvestments(Integer numberOfInvestments) {
         this.numberOfInvestments = numberOfInvestments;
     }
 
-    public double getInvestedAmount() {
+    public Double getInvestedAmount() {
         return investedAmount;
     }
 
-    public void setInvestedAmount(double investedAmount) {
+    public void setInvestedAmount(Double investedAmount) {
         this.investedAmount = investedAmount;
     }
 
-    public double getRealized() {
+    public Double getRealized() {
         return realized;
     }
 
-    public void setRealized(double realized) {
+    public void setRealized(Double realized) {
         this.realized = realized;
     }
 
-    public double getUnrealized() {
+    public Double getUnrealized() {
         return unrealized;
     }
 
-    public void setUnrealized(double unrealized) {
+    public void setUnrealized(Double unrealized) {
         this.unrealized = unrealized;
     }
 
-    public double getDpi() {
+    public Double getDpi() {
         return dpi;
     }
 
-    public void setDpi(double dpi) {
+    public void setDpi(Double dpi) {
         this.dpi = dpi;
     }
 
-    public double getNetIrr() {
+    public Double getNetIrr() {
         return netIrr;
     }
 
-    public void setNetIrr(double netIrr) {
+    public void setNetIrr(Double netIrr) {
         this.netIrr = netIrr;
     }
 
-    public double getNetTvpi() {
+    public Double getNetTvpi() {
         return netTvpi;
     }
 
-    public void setNetTvpi(double netTvpi) {
+    public void setNetTvpi(Double netTvpi) {
         this.netTvpi = netTvpi;
     }
 
-    public double getGrossIrr() {
+    public Double getGrossIrr() {
         return grossIrr;
     }
 
-    public void setGrossIrr(double grossIrr) {
+    public void setGrossIrr(Double grossIrr) {
         this.grossIrr = grossIrr;
     }
 
-    public double getGrossTvpi() {
+    public Double getGrossTvpi() {
         return grossTvpi;
     }
 
-    public void setGrossTvpi(double grossTvpi) {
+    public void setGrossTvpi(Double grossTvpi) {
         this.grossTvpi = grossTvpi;
     }
 
