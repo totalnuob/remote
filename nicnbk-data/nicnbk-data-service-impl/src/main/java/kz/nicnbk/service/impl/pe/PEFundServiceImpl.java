e7ff654eeace5e52d3040d36aa298b8f662d70e4
Pak <alexey.v.pak@gmail.com>
2017-10-13 14:44:42 +0600

    My new message 3

--- nicnbk-data/nicnbk-data-service-impl/src/main/java/kz/nicnbk/service/impl/pe/PEFundServiceImpl.java
+++ nicnbk-data/nicnbk-data-service-impl/src/main/java/kz/nicnbk/service/impl/pe/PEFundServiceImpl.java
@@ -463,18 +463,18 @@ public class PEFundServiceImpl implements PEFundService {
 //
 //        return;
 //    }
-
-
-    public static double[] toPrimitive(Double[] array) {
-        if (array == null) {
-            return null;
-        } else if (array.length == 0) {
-            return EMPTY_DOUBLE_ARRAY;
-        }
-        final double[] result = new double[array.length];
-        for (int i = 0; i < array.length; i++) {
-            result[i] = array[i].doubleValue();
-        }
-        return result;
-    }
+//
+//
+//    public static double[] toPrimitive(Double[] array) {
+//        if (array == null) {
+//            return null;
+//        } else if (array.length == 0) {
+//            return EMPTY_DOUBLE_ARRAY;
+//        }
+//        final double[] result = new double[array.length];
+//        for (int i = 0; i < array.length; i++) {
+//            result[i] = array[i].doubleValue();
+//        }
+//        return result;
+//    }
 }
