a82fdd5e3d0197c77aaf3771f955d272972a9453
Pak <alexey.v.pak@gmail.com>
2017-10-17 11:58:41 +0600

    My new message 3

--- nicnbk-data/nicnbk-data-service-impl/src/main/java/kz/nicnbk/service/converter/pe/PECompanyPerformanceIddEntityConverter.java
+++ nicnbk-data/nicnbk-data-service-impl/src/main/java/kz/nicnbk/service/converter/pe/PECompanyPerformanceIddEntityConverter.java
@@ -3,10 +3,12 @@ package kz.nicnbk.service.converter.pe;
 import kz.nicnbk.repo.model.pe.PECompanyPerformanceIdd;
 import kz.nicnbk.service.converter.dozer.BaseDozerEntityConverter;
 import kz.nicnbk.service.dto.pe.PECompanyPerformanceIddDto;
+import org.springframework.stereotype.Component;
 
 /**
  * Created by Pak on 17.10.2017.
  */
+@Component
 public class PECompanyPerformanceIddEntityConverter extends BaseDozerEntityConverter<PECompanyPerformanceIdd, PECompanyPerformanceIddDto> {
 
     @Override
