79ed16a09c39e69226db0f42d8fd474e2d12dfa6
Pak <alexey.v.pak@gmail.com>
2018-01-24 10:10:44 +0600

    Testing git

--- nicnbk-data/nicnbk-data-service-impl/src/main/java/kz/nicnbk/service/impl/pe/PEPdfServiceImpl.java
+++ nicnbk-data/nicnbk-data-service-impl/src/main/java/kz/nicnbk/service/impl/pe/PEPdfServiceImpl.java
@@ -1,13 +1,15 @@
 package kz.nicnbk.service.impl.pe;
 
 import kz.nicnbk.service.api.pe.PEPdfService;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by Pak on 24/01/2018.
  */
+@Service
 public class PEPdfServiceImpl implements PEPdfService {
     @Override
     public void createOnePager() {
-
+        System.out.println("PDF");
     }
 }
