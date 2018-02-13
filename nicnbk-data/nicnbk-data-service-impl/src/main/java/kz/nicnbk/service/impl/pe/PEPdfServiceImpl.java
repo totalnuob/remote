bfb1e1e115edfe679f4d6208c3c2bece28d697f0
Pak <alexey.v.pak@gmail.com>
2018-02-13 14:59:31 +0600

    Testing git

--- nicnbk-data/nicnbk-data-service-impl/src/main/java/kz/nicnbk/service/impl/pe/PEPdfServiceImpl.java
+++ nicnbk-data/nicnbk-data-service-impl/src/main/java/kz/nicnbk/service/impl/pe/PEPdfServiceImpl.java
@@ -12,6 +12,11 @@ import com.itextpdf.layout.element.List;
 import kz.nicnbk.service.api.pe.PECompanyPerformanceIddService;
 import kz.nicnbk.service.api.pe.PEPdfService;
 import kz.nicnbk.service.dto.pe.PECompanyPerformanceIddDto;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartUtilities;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -85,6 +90,41 @@ public class PEPdfServiceImpl implements PEPdfService {
             document.add(table);
 
             document.close();
+
+            final String fiat = "FIAT";
+            final String audi = "AUDI";
+            final String ford = "FORD";
+            final String speed = "Speed";
+            final String millage = "Millage";
+            final String userrating = "User Rating";
+            final String safety = "safety";
+
+            final DefaultCategoryDataset dataset = new DefaultCategoryDataset( );
+            dataset.addValue( 1.0 , fiat , speed );
+            dataset.addValue( 3.0 , fiat , userrating );
+            dataset.addValue( 5.0 , fiat , millage );
+            dataset.addValue( 5.0 , fiat , safety );
+
+            dataset.addValue( 5.0 , audi , speed );
+            dataset.addValue( 6.0 , audi , userrating );
+            dataset.addValue( 10.0 , audi , millage );
+            dataset.addValue( 4.0 , audi , safety );
+
+            dataset.addValue( 4.0 , ford , speed );
+            dataset.addValue( 2.0 , ford , userrating );
+            dataset.addValue( 3.0 , ford , millage );
+            dataset.addValue( 6.0 , ford , safety );
+
+            JFreeChart barChart = ChartFactory.createBarChart(
+                    "CAR USAGE STATIStICS",
+                    "Category", "Score",
+                    dataset, PlotOrientation.VERTICAL,
+                    true, true, false);
+
+            int width = 640;    /* Width of the image */
+            int height = 480;   /* Height of the image */
+            File BarChart = new File( "nicnbk-data/nicnbk-data-service-impl/src/main/resources/BarChart.jpeg" );
+            ChartUtilities.saveChartAsJPEG( BarChart , barChart , width , height );
         } catch (IOException ex) {
             logger.error("Error creating PE fund's One Pager: " + fundId, ex);
         }
