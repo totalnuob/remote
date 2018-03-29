8612255450cfc0dc0700d8e91df0a1e6a9602071
Pak <alexey.v.pak@gmail.com>
2018-03-30 01:16:06 +0600

    Testing git

--- nicnbk-data/nicnbk-data-ws-impl/src/main/java/kz/nicnbk/ws/rest/PrivateEquityFirmServiceREST.java
+++ nicnbk-data/nicnbk-data-ws-impl/src/main/java/kz/nicnbk/ws/rest/PrivateEquityFirmServiceREST.java
@@ -3,6 +3,7 @@ package kz.nicnbk.ws.rest;
 import kz.nicnbk.service.api.authentication.TokenService;
 import kz.nicnbk.service.api.pe.PEFirmService;
 import kz.nicnbk.service.api.pe.PEFundService;
+import kz.nicnbk.service.api.pe.PEIrrService;
 import kz.nicnbk.service.dto.files.FilesDto;
 import kz.nicnbk.service.dto.pe.PEFirmDto;
 import kz.nicnbk.service.dto.pe.PEFundDto;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
@@ -32,6 +34,9 @@ public class PrivateEquityFirmServiceREST extends CommonServiceREST{
 
     @Autowired
     private PEFundService peFundService;
+
+    @Autowired
+    private PEIrrService irrService;
 
     @Autowired
     private TokenService tokenService;
@@ -63,7 +68,29 @@ public class PrivateEquityFirmServiceREST extends CommonServiceREST{
     @PreAuthorize("hasRole('ROLE_PRIVATE_EQUITY_VIEWER') OR hasRole('ROLE_PRIVATE_EQUITY_EDITOR') OR hasRole('ROLE_ADMIN')")
     @RequestMapping(value = "/getTotalIrrForOnePager/{id}", method = RequestMethod.GET)
     public ResponseEntity getTotalIrrForOnePager(@PathVariable long id){
-        Double totalIrr = 1.0;
+
+        Double totalIrr = null;
+
+        List<PEFundDto> fundDtoList = this.peFundService.loadFirmFunds(id, false);
+        List<PEFundDto> fundDtoListShort = new ArrayList<>();
+        boolean areAllKeyFundStatisticsCalculatedByGrossCF = true;
+
+        if (fundDtoList != null) {
+            for (PEFundDto fundDto : fundDtoList) {
+                if (fundDto.getDoNotDisplayInOnePager() != null && fundDto.getDoNotDisplayInOnePager()) {continue;}
+
+                if (fundDto.getCalculationType() == null || fundDto.getCalculationType() !=2) {
+                    areAllKeyFundStatisticsCalculatedByGrossCF = false;
+                }
+
+                fundDtoListShort.add(fundDto);
+            }
+
+            if (areAllKeyFundStatisticsCalculatedByGrossCF) {
+                totalIrr  = irrService.getIrrByFundList(fundDtoListShort);
+            }
+        }
+
         if(totalIrr != null){
             return new ResponseEntity<>(totalIrr, null, HttpStatus.OK);
         }else{
