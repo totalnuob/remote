c9988118e7631053bda44a6dabc1b3e4644c94b6
timur <magzumov@nicnbk.kz>
2018-10-31 12:32:02 +0600

    Insert script for ic_meeting_topic_type

---
+++ nicnbk-data/nicnbk-data-repo-impl/src/main/resources/database/sql/V0.0073_insert_table_ic_meering_topic_type.sql
@@ -0,0 +1,6 @@
+INSERT INTO public.ic_meeting_topic_type(id, code, name_en, name_kz, name_ru) VALUES (1, 'HF', 'Hedge Funds', null, '');
+INSERT INTO public.ic_meeting_topic_type(id, code, name_en, name_kz, name_ru) VALUES (2, 'PE', 'Private Equity', null, '');
+INSERT INTO public.ic_meeting_topic_type(id, code, name_en, name_kz, name_ru) VALUES (3, 'RE', 'Real Estate', null, '');
+INSERT INTO public.ic_meeting_topic_type(id, code, name_en, name_kz, name_ru) VALUES (4, 'SRM', 'Strategy and Risks', null, '');
+INSERT INTO public.ic_meeting_topic_type(id, code, name_en, name_kz, name_ru) VALUES (5, 'REP', 'Reporting', null, '');
+
