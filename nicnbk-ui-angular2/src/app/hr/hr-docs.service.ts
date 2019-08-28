0b7b2ffd1a1aefa0266833792d9ab7f7fa3fbe86
alexey <pak@nicnbk.kz>
2019-08-28 16:43:28 +0600

    HR Docs Service

---
+++ nicnbk-ui-angular2/src/app/hr/hr-docs.service.ts
@@ -0,0 +1,29 @@
+import {CommonService} from "../common/common.service";
+import {Injectable} from "@angular/core";
+import {DATA_APP_URL} from "../common/common.service.constants";
+import {FileUploadService} from "../upload/file.upload.service";
+import {Http} from "@angular/http";
+
+@Injectable()
+export class HrDocsService extends CommonService {
+    private HR_DOCS_BASE_URL = DATA_APP_URL + "hrdocs/";
+    private HR_DOCS_GET_URL = this.HR_DOCS_BASE_URL + "get/";
+    private HR_DOCS_UPLOAD_URL = this.HR_DOCS_BASE_URL + "upload/";
+
+    constructor (
+        private uploadService: FileUploadService,
+        private http: Http)
+    {
+        super();
+    }
+
+    get() {
+        return this.http.get(this.HR_DOCS_GET_URL, this.getOptionsWithCredentials())
+            .map(this.extractData)
+            .catch(this.handleErrorResponse);
+    }
+
+    postFiles(files) {
+        return this.uploadService.postFiles(this.HR_DOCS_UPLOAD_URL, [], files, null);
+    }
+}
