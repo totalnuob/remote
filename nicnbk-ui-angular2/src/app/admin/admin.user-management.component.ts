4c4c0d3c66135b97b7fd8fe5b0a08010d693d3a4
alexey <pak@nicnbk.kz>
2020-02-04 17:59:43 +0600

    User management component, blank employee profile for administration.

--- nicnbk-ui-angular2/src/app/admin/admin.user-management.component.ts
+++ nicnbk-ui-angular2/src/app/admin/admin.user-management.component.ts
@@ -1,33 +1,72 @@
-import { Component, OnInit} from '@angular/core';
-import {ActivatedRoute, Router} from '@angular/router';
-import {AuthenticationService} from "../authentication/authentication.service";
-import {Subscription} from 'rxjs';
-import {ModuleAccessCheckerService} from "../authentication/module.access.checker.service";
+import {Component} from '@angular/core';
 import {CommonFormViewComponent} from "../common/common.component";
-
-declare var $: any
+import {ActivatedRoute, Router} from "@angular/router";
+import {EmployeesSearchParams} from "../employee/model/employees-search-params";
+import {Subscription} from "rxjs";
+import {EmployeeService} from "../employee/employee.service";
+import {EmployeesSearchResult} from "../employee/model/employees-search-result";
+import {ErrorResponse} from "../common/error-response";
 
 @Component({
     selector: 'admin-user-management',
     templateUrl: 'view/admin.user-management.component.html',
-    styleUrls: [
-    ],
-    providers: [AuthenticationService]
+    styleUrls: [],
+    providers: []
 })
-export class AdminUserManagementComponent extends CommonFormViewComponent implements OnInit{
+export class AdminUserManagementComponent extends CommonFormViewComponent{
+
+    private sub: any;
+    private searchParams = new EmployeesSearchParams();
+    private searchResult: EmployeesSearchResult;
+
     busy: Subscription;
 
-    private moduleAccessChecker: ModuleAccessCheckerService;
-    private sub: any;
-
-    ngOnInit():void {
-    }
-
     constructor(
+        private employeeService: EmployeeService,
         private router: Router,
         private route: ActivatedRoute
     ){
         super(router);
-        this.moduleAccessChecker = new ModuleAccessCheckerService;
+
+        this.sub = this.route
+            .params
+            .subscribe(params => {
+                if(params['params'] != null) {
+                    this.searchParams = JSON.parse(params['params']);
+                } else {
+                    this.searchParams.pageSize = 20;
+                    this.searchParams.page = 0;
+                }
+                console.log(this.searchParams);
+                this.search(this.searchParams.page);
+            });
+    }
+
+    search(page){
+        this.searchParams.page = page;
+
+        this.busy = this.employeeService.search(this.searchParams)
+            .subscribe(
+                response => {
+                    this.searchResult = response;
+                    console.log(response);
+                },
+                (error: ErrorResponse) => {
+                    this.errorMessage = "Error searching employees";
+                    console.log("Error searching employees");
+                    if(error && !error.isEmpty()){
+                        this.processErrorMessage(error);
+                        console.log(error);
+                    }
+                    this.postAction(null, this.errorMessage);
+                }
+            );
+    }
+
+    navigate(username){
+        // console.log(this.searchParams);
+        // this.searchParams.path = '/hr/employees';
+        // let params = JSON.stringify(this.searchParams);
+        // this.router.navigate(['/profile', username, { params }]);
     }
 }
