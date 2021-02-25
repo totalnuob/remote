f3ba9a11903cfff21948662c0221f113dd72ccb5
timur <magzumov@nicnbk.kz>
2021-02-25 12:17:14 +0600

    NB Reporting: added NICK MF expenses for correct calculation of capital

--- nicnbk-data/nicnbk-data-repo-impl/src/main/resources/database/sql/V0.0041_insert-table-reserve-calculation-expense-type.sql
+++ nicnbk-data/nicnbk-data-repo-impl/src/main/resources/database/sql/V0.0041_insert-table-reserve-calculation-expense-type.sql
@@ -1,6 +1,7 @@
 INSERT INTO rep_reserve_calc_expense_type(id, code, name_en, name_ru, name_kz) VALUES(1, 'ADD', null, 'Пополнение капитала', '');
 INSERT INTO rep_reserve_calc_expense_type(id, code, name_en, name_ru, name_kz) VALUES(2, 'RETURN', null, 'Возврат капитала', '');
 INSERT INTO rep_reserve_calc_expense_type(id, code, name_en, name_ru, name_kz) VALUES(3, 'ADM_FEE', null, 'Комиссия', '');
+INSERT INTO rep_reserve_calc_expense_type(id, code, name_en, name_ru, name_kz) VALUES(4, 'NMF_EXP', null, 'Пополнение капитала на расходы NICK MF', '');
 
 
 
